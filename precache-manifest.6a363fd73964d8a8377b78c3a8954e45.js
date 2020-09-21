25cf8446a8a0dfd797552ef9386d55552e9693c3
ariefyud <ariefyud12@gmail.com>
2020-09-21 11:45:49 +0700

    Updates

---
+++ precache-manifest.6a363fd73964d8a8377b78c3a8954e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6afe06bf55afbb61c3715599fd0066",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "162faa048edaf3adf97c",
+    "url": "/react-todo-list/static/css/main.0be5382c.chunk.css"
+  },
+  {
+    "revision": "3aad58ab1e10533cbbb4",
+    "url": "/react-todo-list/static/js/2.c2c22a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.c2c22a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162faa048edaf3adf97c",
+    "url": "/react-todo-list/static/js/main.5a8ec41b.chunk.js"
+  },
+  {
+    "revision": "fc4261b85325ca142166",
+    "url": "/react-todo-list/static/js/runtime-main.5b00e47c.js"
+  }
+]);
